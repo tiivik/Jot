8f6e7b62d8d5f3929ab449a40073f578fbdc6c7d
Rainer Selvet <rainerselvet@gmail.com>
2020-03-21 15:21:02 +0200

    Initial Commit

---
+++ Snip/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Snip
+//
+//  Created by Rainer Selvet on 21/03/2020.
+//  Copyright © 2020 Rainer Selvet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
